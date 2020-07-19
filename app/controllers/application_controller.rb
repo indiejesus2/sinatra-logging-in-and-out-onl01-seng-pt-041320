7578f2b6351e3be1cf3b07d9d966b7492f243be3
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-07-19 00:03:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,32 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(params)
+    # binding.pry
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
 
+    
   end
 
   get '/account' do
+    @user = Helpers.current_user(session)
+    # binding.pry
+    if Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
